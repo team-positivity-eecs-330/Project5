da9077b11385fb19df74ee4c60c8c2eff8f6c89f
katherine-mcilree <katherinemcilree2017@u.northwestern.edu>
2019-03-07 15:47:17 -0600

    changes

--- javascript/pos.js
+++ javascript/pos.js
@@ -8,6 +8,19 @@ function pointEarned(){
 }
 
 function submitText(){
-  document.getElementById("forumText").innerHTML += ("\n" + document.getElementById("forumNewText").value) ;
+  // document.getElementById("forumText").innerHTML += ("\n" + document.getElementById("forumNewText").value) ;
   pointEarned();
+  newComment();
+  document.getElementById("forumNewText").value = '';
+}
+
+function newComment(){
+  var commentDiv = document.createElement("div");
+  commentDiv.id = "forumText"
+  var userCont = document.createTextNode("come on");
+  commentDiv.appendChild(userCont);
+  var element = document.getElementById("forumContain");
+  console.log(typeof(element))
+  console.log("YO")
+  element.appendChild(commentDiv);
 }
