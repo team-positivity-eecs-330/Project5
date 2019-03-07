ca52b8cdfa8b69c91fe77c9950f26f0bb082580a
katherine-mcilree <katherinemcilree2017@u.northwestern.edu>
2019-03-06 18:08:16 -0600

    Connect changes

--- javascript/pos.js
+++ javascript/pos.js
@@ -6,3 +6,8 @@ function goBack(){
 function pointEarned(){
   alert("YAY! You earned 100 points!")
 }
+
+function submitText(){
+  document.getElementById("forumText").innerHTML += ("\n" + document.getElementById("forumNewText").value) ;
+  pointEarned();
+}
