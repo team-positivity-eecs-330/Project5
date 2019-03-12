e5656ecb4ccf88256d5941385cc9b705b894efff
katherine-mcilree <katherinemcilree2017@u.northwestern.edu>
2019-03-12 17:04:49 -0500

    Changes

--- javascript/pos.js
+++ javascript/pos.js
@@ -1,21 +1,58 @@
+var popup_viz = false;
+var info_viz = false;
+var overlay_viz = false;
+
 function goBack(){
   window.history.back();
   console.log("funtion connected")
 }
 
-function pointEarned(){
-  //alert("YAY! You earned 100 points!");
-  document.getElementById("commentPoints").style.display="inline-block";
+
+function toggle_viz(){
+  popup_viz = !popup_viz;
+  var toggle = "hidden"
+  if(popup_viz){
+    toggle = "visible"
+  }
+
+  document.getElementById("commentPoints").style.visibility = toggle;
+  document.getElementById("overlay").style.visibility = toggle;
 }
 
-function hidePopup(){
-  document.getElementById("commentPoints").style.display="none";
+function toggle_info_viz(){
+  info_viz = !info_viz;
+  var toggle = "hidden"
+  if(info_viz){
+    toggle = "visible"
+  }
+  document.getElementById("badgeInfo").style.visibility = toggle;
+  document.getElementById("overlay").style.visibility = toggle;
+}
+
+function toggleOverlay(){
+  if (popup_viz){
+    popup_viz = !popup_viz;
+    document.getElementById("commentPoints").style.visibility = "hidden";
+
+  }
+  if (info_viz){
+    info_viz = !info_viz;
+    document.getElementById("badgeInfo").style.visibility = "hidden";
+  }
+  overlay_viz = !overlay_viz;
+  document.getElementById("overlay").style.visibility = "hidden";
+
 }
 
 function submitText(){
   // document.getElementById("forumText").innerHTML += ("\n" + document.getElementById("forumNewText").value) ;
-  pointEarned();
-  newComment();
+  if(document.getElementById("forumNewText").value){
+      newComment();
+      toggle_viz();
+  }
+  else {
+    alert("Please Enter Text")
+  }
   document.getElementById("forumNewText").value = '';
 }
 
@@ -30,7 +67,7 @@ function newComment(){
 }
 
 // For todays date;
-Date.prototype.today = function () { 
+Date.prototype.today = function () {
   return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
 }
 
